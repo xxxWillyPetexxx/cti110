15b4c4dfe668702f98527f86ce0774b4a18139f3
xxxWillyPetexxx <113489250+xxxWillyPetexxx@users.noreply.github.com>
2022-10-06 21:53:49 -0400

    Add files via upload

---
+++ P2LAB_Stats_WOODJOSHUA.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+num_prod = num1 * num2 * num3 * num4 
+num_avg = ((num1 + num2 + num3 + num4) /4)
+
+print(f'{num_prod:.0f} {num_avg:.0f}')
+print(f'{num_prod:.3f} {num_avg:.3f}')
