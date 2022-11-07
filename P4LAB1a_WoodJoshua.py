0ca8d293911975baa3ba4ace8b17cd1d5a8f0d23
xxxWillyPetexxx <113489250+xxxWillyPetexxx@users.noreply.github.com>
2022-11-06 20:46:17 -0500

    Add files via upload

---
+++ P4LAB1a_WoodJoshua.py
@@ -0,0 +1,14 @@
+import turtle
+wn = turtle.Screen()
+josh = turtle.Turtle()
+
+for i in range(4):
+    josh.forward(120)
+    josh.left(90)
+
+for i in range(3):
+    josh.forward(120)
+    josh.left(120)
+
+wn.mainloop()
+
